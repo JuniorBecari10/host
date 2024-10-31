64ff9c3e73916c45326e19060a3ac318d70f7b5b
Antônio Carlos <antonioocarlos@proton.me>
2024-10-31 16:33:09 -0300

    fix: the default port is 8080 now

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const express = require("express");
 const init = require("./init");
 const setupRoutes = require("./routes/routes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 init(app);
