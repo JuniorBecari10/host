4cef46b1f973fce05972bb9f4f99f27c17444572
Antônio Carlos <antonioocarlos@proton.me>
2024-11-03 21:25:53 -0300

    minor: removed periodic checks for JWT token, changed payments buttons' brightness and a minor change

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -1,41 +1,8 @@
-function check(alert) {
-    if (alert) {
-        // if the token is missing or is invalid, redirect to login, since this request will fail if that happens
-        getUser()
-            .catch((e) => {
-                if (alert) {
-                    swal({
-                        title: 'Token de acesso expirado ou excluído',
-                        text: 'Você será redirecionado para a tela de login.',
-                        icon: 'warning',
-                        dangerMode: 'true',
-                    })
-                        .then(async (_) => {
-                            window.location.href = `http://${API_URL}/login`;
-                        });
-                }
-                else
-                    window.location.href = `http://${API_URL}/login`;
-            });
-    }
+// if the token is missing or is invalid, redirect to login, since this request will fail if that happens
+getUser().catch((_) => {
+    window.location.href = `http://${API_URL}/login`;
+});
 
-    // fallback to redirect to login if the token is missing
-    if (!localStorage.getItem("token")) {
-        if (alert) {
-            swal({
-                title: 'Token de acesso expirado ou excluído',
-                text: 'Você será redirecionado para a tela de login.',
-                icon: 'information',
-                dangerMode: 'true',
-            })
-                .then(async (_) => {
-                    window.location.href = `http://${API_URL}/login`;
-            });
-        }
-        else
-            window.location.href = `http://${API_URL}/login`;
-    }
-}
-
-setInterval(() => check(true), 10000); // run every 10 seconds
-check(false); // run once now
+// fallback to redirect to login if the token is missing
+if (!localStorage.getItem("token"))
+    window.location.href = `http://${API_URL}/login`;
--- src/util.js
+++ src/util.js
@@ -37,7 +37,7 @@ function logMessage(req) {
     const now = new Date();
         
     const day = [
-        now.getDate() < 10 ? "0" + now.getDate() : now.getDate(),  // Use getDate() here
+        now.getDate() < 10 ? "0" + now.getDate() : now.getDate(),
         now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1,
         now.getFullYear().toString(),
     ];
